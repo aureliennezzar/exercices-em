514737f792ad8e6ee0f3453a75080610b2f1f936
aureliennezzar <aurelien.nezzar@outlook.fr>
2021-01-20 16:29:41 +0100

    fonts added, js init, css update

--- bloc1.4/formulaire/assets/js/app.js
+++ bloc1.4/formulaire/assets/js/app.js
@@ -1,7 +1,14 @@
 document.addEventListener('DOMContentLoaded', function () {
 	var app = {
-		init:function(){
+		init: function () {
 			console.log('STARTER');
+			app.formSubmit()
+		},
+		formSubmit: function () {
+			const form = document.querySelector('.form')
+			form.addEventListener('submit', function (e) {
+				e.preventDefault()
+			})
 		}
 	}
 	app.init()
